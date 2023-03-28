bf655384186e83fc5be1d1662d36b01984ead7c6
tsharksss <tsharksss@gmail.com>
2023-03-28 11:04:10 +0300

    fix: archive search

--- frontend/src/components/Archive.jsx
+++ frontend/src/components/Archive.jsx
@@ -1,31 +1,43 @@
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {fetchArchive} from "../redux/slices/slices";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchArchive } from "../redux/slices/slices";
 
 
 const Archive = () => {
 
   const dispatch = useDispatch()
-  const {archive} = useSelector(state => state.archive)
+  const { archive } = useSelector(state => state.archive)
 
   const [search, setSearch] = useState('')
   const [currentDate, setCurrentDate] = useState('')
 
   const isArchiveLoading = archive.status === 'loading'
 
-  let resultSearch = (archive.items).filter(item => item.client.toLowerCase().includes(search.toLowerCase()))
-  const [data, setData] = useState(resultSearch)
+  let resultSearch
+  const [data, setData] = useState([])
 
   useEffect(() => {
     dispatch(fetchArchive())
+    setData(archive.items)
   }, [])
 
   useEffect(() => {
     if (currentDate != '') {
-      let resultSearchDate = (archive.items).filter(item => new Intl.DateTimeFormat().format(new Date(item.createdAt)).includes(new Date(Date.parse(currentDate)).toLocaleDateString()))
+      let resultSearchData = (archive.items).filter(item => item.client.toLowerCase().includes(search.toLowerCase()))
+            .filter(item => new Intl.DateTimeFormat().format(new Date(item.createdAt)).includes(new Date(Date.parse(currentDate)).toLocaleDateString()))
+      setData(resultSearchData)
+    } else {
+      let resultSearchData = (archive.items).filter(item => item.client.toLowerCase().includes(search.toLowerCase()))
+      setData(resultSearchData)
+    }
+  }, [search])
+
+  useEffect(() => {
+    if (currentDate != '') {
+      let resultSearchDate = (data).filter(item => new Intl.DateTimeFormat().format(new Date(item.createdAt)).includes(new Date(Date.parse(currentDate)).toLocaleDateString()))
       setData(resultSearchDate)
     } else {
-      setData(resultSearch)
+      setData(archive.items)
     }
   }, [currentDate])
 
@@ -37,39 +49,42 @@ const Archive = () => {
             <h2>Архив</h2>
           </div>
           <input type="text" placeholder='Поиск по заказчику' className='search'
-                 onChange={e => setSearch(e.target.value)}/>
+            value={search} onChange={e => setSearch(e.target.value)} />
           <div className="filter_dates">
             <div className="block_date">
               <label>Дата</label>
-              <input type="date" className='datepicker' value={currentDate} onChange={event => setCurrentDate(event.target.value)}/>
-              <button onClick={() => setCurrentDate('')}>Очистить</button>
+              <input type="date" className='datepicker' value={currentDate} onChange={event => setCurrentDate(event.target.value)} />
+              <button onClick={() => {
+                setCurrentDate('')
+                setSearch('')
+              }}>Очистить</button>
             </div>
           </div>
           <table>
             <thead>
-            <tr>
-              <th>Вид</th>
-              <th>Артикул</th>
-              <th>Аромат</th>
-              <th>Кол-во</th>
-              <th>Куда</th>
-              <th>Дата</th>
-              <th>Пользователь</th>
-            </tr>
+              <tr>
+                <th>Вид</th>
+                <th>Артикул</th>
+                <th>Аромат</th>
+                <th>Кол-во</th>
+                <th>Куда</th>
+                <th>Дата</th>
+                <th>Пользователь</th>
+              </tr>
             </thead>
             <tbody>
-            {(isArchiveLoading ? [...Array(5)] : data).map((obj, index) => isArchiveLoading ? 'loading'
-              :
-              <tr key={index}>
-                <td>{obj.flavoring.typeFlavoring.name}</td>
-                <td>{obj.flavoring.vendor_code}</td>
-                <td>{obj.flavoring.name}</td>
-                <td>{obj.count}</td>
-                <td>{obj.client}</td>
-                <td>{new Intl.DateTimeFormat().format(new Date(obj.createdAt))}</td>
-                <td>{obj.user.name}</td>
-              </tr>
-            )}
+              {(isArchiveLoading ? [...Array(5)] : data).map((obj, index) => isArchiveLoading ? 'loading'
+                :
+                <tr key={index}>
+                  <td>{obj.flavoring.typeFlavoring.name}</td>
+                  <td>{obj.flavoring.vendor_code}</td>
+                  <td>{obj.flavoring.name}</td>
+                  <td>{obj.count}</td>
+                  <td>{obj.client}</td>
+                  <td>{new Intl.DateTimeFormat().format(new Date(obj.createdAt))}</td>
+                  <td>{obj.user.name}</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
