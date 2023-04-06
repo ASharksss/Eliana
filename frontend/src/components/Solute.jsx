28a1b0f81dd273204833349ec62b1f2277a11728
tsharksss <tsharksss@gmail.com>
2023-04-06 14:04:47 +0300

    feat: zeroing solute

--- frontend/src/components/Solute.jsx
+++ frontend/src/components/Solute.jsx
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {NavLink} from "react-router-dom";
-import {fetchConsumables, fetchSolutions} from "../redux/slices/slices";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { NavLink } from "react-router-dom";
+import { fetchSolutions } from "../redux/slices/slices";
+import { useDispatch, useSelector } from "react-redux";
+import axios from '../axios'
 
 const Solute = () => {
   const dispatch = useDispatch()
-  const {solutions} = useSelector(state => state.solutions)
+  const { solutions } = useSelector(state => state.solutions)
+  const { user } = useSelector(state => state.user)
   const [search, setSearch] = useState('')
 
   const isSolutionsLoading = solutions.status === 'loading'
@@ -13,6 +15,21 @@ const Solute = () => {
   useEffect(() => {
     dispatch(fetchSolutions())
   }, [])
+
+  const removeSolute = (id, name, percent) => {
+    const check = window.confirm('Точно хотите обнулить - ' + name + ' ' + percent + '%')
+    if (check) {
+      axios.delete('/api/user/zeroingSolutionLiter?sId=' + id + '&userId' + user.data.id)
+        .then(res => {
+          dispatch(fetchSolutions())
+          alert('Значение обнулено')
+        })
+        .catch(err => {
+          alert('Ошибка обработки')
+          console.log(err)
+        })
+    }
+  }
 
   let resultSearch = (solutions.items).filter(item => item.aroma.toLowerCase().includes(search.toLowerCase()))
 
@@ -29,43 +46,51 @@ const Solute = () => {
               </button>
             </NavLink>
           </div>
-          <input type="text" placeholder='Поиск' className='search' onChange={e => setSearch(e.target.value)}/>
+          <input type="text" placeholder='Поиск' className='search' onChange={e => setSearch(e.target.value)} />
           <table>
             <thead>
-            <tr>
-              <th>%</th>
-              <th>Аромат</th>
-              <th>Отдушка</th>
-              <th>ПГ и ПЭГ</th>
-              <th>Литры</th>
-              <th>Дата</th>
-            </tr>
+              <tr>
+                <th>%</th>
+                <th>Аромат</th>
+                <th>Отдушка</th>
+                <th>ПГ и ПЭГ</th>
+                <th>Литры</th>
+                <th>Дата</th>
+                <th>Действия</th>
+              </tr>
 
             </thead>
             <tbody>
-            {(isSolutionsLoading ? [...Array(5)] : resultSearch).map((obj, index) => isSolutionsLoading ? 'loading'
-              :
+              {(isSolutionsLoading ? [...Array(5)] : resultSearch).map((obj, index) => isSolutionsLoading ? 'loading'
+                :
 
-              <tr key={index}>
-                <td>{obj.percent_solution}</td>
-                <td>{obj.aroma}</td>
-                <td>{JSON.parse(obj.perfume).map((item, index_perfume) => (
-                  <tr key={index_perfume}>
-                    <td>{item.name}</td>
-                    <td>{item.count} кг.</td>
-                  </tr>
-                ))}</td>
-                <td>{JSON.parse(obj.consumable).map((item, index_consumable) => (
-                  <tr key={index_consumable}>
-                    <td>{item.name}</td>
-                    <td>{item.count} кг.</td>
-                  </tr>
-                ))}</td>
-                <td>{obj.liter}</td>
-                <td>
-                  {new Intl.DateTimeFormat().format(new Date(obj.createdAt))}
-                </td>
-              </tr>)}
+                <tr key={index}>
+                  <td>{obj.percent_solution}</td>
+                  <td>{obj.aroma}</td>
+                  <td>{JSON.parse(obj.perfume).map((item, index_perfume) => (
+                    <tr key={index_perfume}>
+                      <td>{item.name}</td>
+                      <td>{item.count} кг.</td>
+                    </tr>
+                  ))}</td>
+                  <td>{JSON.parse(obj.consumable).map((item, index_consumable) => (
+                    <tr key={index_consumable}>
+                      <td>{item.name}</td>
+                      <td>{item.count} кг.</td>
+                    </tr>
+                  ))}</td>
+                  <td>{parseFloat(obj.liter).toFixed(2)}</td>
+                  <td>
+                    {new Intl.DateTimeFormat().format(new Date(obj.createdAt))}
+                  </td>
+                  <td><button onClick={() => removeSolute(obj.id, obj.aroma, obj.percent_solution)}
+                    style={{
+                      cursor: 'pointer',
+                      border: 'none',
+                      padding: '5px',
+                      borderRadius: '2px'
+                    }} type='button'>Обнулить</button></td>
+                </tr>)}
             </tbody>
           </table>
         </div>
