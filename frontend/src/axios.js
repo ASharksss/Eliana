27f7861a2ec5dfbb2fea1fa5540e7860f6da191d
ASharkss <alsukurbanalieva035@gmail.com>
2023-06-06 15:48:23 +0300

    supplies

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
  //baseURL: 'https://backend-production-e793.up.railway.app/'
- //baseURL: 'http://localhost:5000/'
- baseURL: 'https://vezdesens.ru/'
+ baseURL: 'http://localhost:5000/'
+ //baseURL: 'https://vezdesens.ru/'
 
 })
 
